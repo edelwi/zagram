f1f5907e88cea8296ea1582c78b26c0a6da4e8c8
Evgeniy Semenov <all_post@mail.ru>
2020-07-13 11:15:43 +0300

    Add example config module.

---
+++ tele_config_example.py
@@ -0,0 +1,8 @@
+TELEGRAM_TOKEN = 'replace_it_with_your_API_token'
+IS_DEBUG = True
+LOG_FILE_NAME = '/var/log/telega.log'
+LOG_FILE_MAXBYTES = 1024000
+LOG_FILE_BACKUP_COUNT = 10
+# Use proxyes dictiotary like in https://requests.readthedocs.io/en/master/user/advanced/#proxies
+# or empty Dict ({}) if proxy is not needed.
+PROXIES = {}
